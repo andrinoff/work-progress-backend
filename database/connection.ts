20f383a9e11e2fdaa4061f095c270c051193516b
Andrey Smirnoff <andrinoff.69@gmail.com>
2025-04-28 13:22:29 +0400

    fsaldkfasjdfls

--- database/connection.ts
+++ database/connection.ts
@@ -11,7 +11,7 @@ const connection = mysql.createConnection({
 var sql = `CREATE TABLE IF NOT EXISTS users (
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    api_key VARCHAR(255) PRIMARY KEY,
+    api_key VARCHAR(255) PRIMARY KEY
 )`;
 const createTable = ()    => {
     connection.connect()
