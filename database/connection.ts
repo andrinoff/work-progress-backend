f6198277d079895addebfefa97fee1997f76e746
Andrey Smirnoff <andrinoff.69@gmail.com>
2025-05-02 16:42:57 +0400

    fix: Added github pages

--- database/connection.ts
+++ database/connection.ts
@@ -11,8 +11,11 @@ const connection = mysql.createConnection({
 var sql = `CREATE TABLE IF NOT EXISTS users (
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    api_key VARCHAR(255) PRIMARY KEY
+    api_key VARCHAR(255) PRIMARY KEY,
 )`;
+
+
+
 const createTable = (): Promise<void> => {
     return new Promise((resolve, reject) => {
         connection.query(sql, function (err, result) {
